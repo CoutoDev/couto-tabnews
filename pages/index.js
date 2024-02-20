5a38b3c08e0253d309eead712708f0fa78c681bd
CoutoDev <c.svitor@yahoo.com.br>
2024-02-20 01:12:37 -0300

    chore: add and run initial prettier

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>
-      Hello There
-    </div>
-  )
-}
+  return <div>Hello There</div>;
+};
 
-export default Home
+export default Home;
