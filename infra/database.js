b43a033d1dd054b257722c4ca833118e3be87f7c
CoutoDev <c.svitor@yahoo.com.br>
2024-03-03 14:22:21 -0300

    feat: add a try catch on database connections

--- infra/database.js
+++ infra/database.js
@@ -1,23 +1,23 @@
 import { Client } from "pg";
 
 async function query(queryObject) {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
+    port: process.env.POSTGRES_PORT,
+  });
+  await client.connect();
+
   try {
-    const client = new Client({
-      user: process.env.POSTGRES_USER,
-      host: process.env.POSTGRES_HOST,
-      database: process.env.POSTGRES_DB,
-      password: process.env.POSTGRES_PASSWORD,
-      port: process.env.POSTGRES_PORT,
-    });
-    await client.connect();
-
     const res = await client.query(queryObject);
-
-    await client.end();
 
     return res;
   } catch (error) {
     throw error;
+  } finally {
+    await client.end();
   }
 }
 export default {
